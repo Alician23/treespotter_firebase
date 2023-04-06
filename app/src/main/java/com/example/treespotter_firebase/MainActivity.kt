8dcc4754e1fa004f427f98f41c55c557026f9192
Alicia <Alicia@gmail.com>
2023-04-05 22:55:54 -0500

    Add database variable

--- app/src/main/java/com/example/treespotter_firebase/MainActivity.kt
+++ app/src/main/java/com/example/treespotter_firebase/MainActivity.kt
@@ -2,10 +2,22 @@ package com.example.treespotter_firebase
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.google.firebase.ktx.Firebase
+import java.util.Date
+
+
+//private val Firebase.firestore: Any
+//    get() {}
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val db = Firebase.firestore
+
+        val tree = mapOf("name" to "pine", "dateSpotter" to Date())
+
+        db.collection("tree").add(tree)
     }
 }
