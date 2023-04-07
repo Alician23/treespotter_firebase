0bd69d7446f7d9cc9c854ced4307f3fa9d1ccbfa
Alicia <Alicia@gmail.com>
2023-04-07 00:11:02 -0500

    Add additional codes

--- app/src/main/java/com/example/treespotter_firebase/MainActivity.kt
+++ app/src/main/java/com/example/treespotter_firebase/MainActivity.kt
@@ -1,12 +1,15 @@
 package com.example.treespotter_firebase
 
+import android.app.DownloadManager.Query
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import androidx.compose.foundation.layout.Direction
 import com.google.firebase.firestore.ktx.firestore
 import com.google.firebase.ktx.Firebase
 import java.util.Date
 
-
+private const val TAG = "MAIN_ACTIVITY"
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -15,8 +18,36 @@ class MainActivity : AppCompatActivity() {
         val db = Firebase.firestore
 
 //        val tree = mapOf("name" to "pine", "dateSpotter" to Date())
-        val tree = mapOf("name" to "oak", "dateSpotter" to Date())
+//        db.collection("tree").add(tree)
+//        val tree2 = mapOf("name" to "oak", "dateSpotter" to Date())
+//        db.collection("tree").add(tree2)
 
-        db.collection("tree").add(tree)
+
+//        val tree = Tree("Pine", Date())
+//        db.collection("trees").add(tree)
+
+        db.collection("trees")
+            .whereEqualTo("name","Pine") // Filter string with Pine trees
+            .whereEqualTo("favorite", true)
+            .orderBy("dateSpotted", Query.Direction.DESCENDING) // Sorting data and composite index -Video 3/10 and 24:19
+            .limit(3)
+            .addSnapshotListener{  treeDocuments, error ->
+
+            if (error != null) {
+                Log.e(TAG, "Error getting all trees", error)
+
+            }
+            if ( treeDocuments != null) {
+                for (treeDoc in treeDocuments) {
+
+                 val treeFromFirebase = treeDoc.toObject(Tree::class.java)
+//                    val name = treeDoc["name"]
+//                    val dateSpotted = treeDoc["dateSpotted"]
+//                    val favorite = treeDoc["favorite"]
+                    val path = treeDoc.reference.path
+                    Log.d(TAG, "$treeFromFirebase, $path")
+                }
+            }
+        }
     }
 }
---
+++ app/src/main/java/com/example/treespotter_firebase/Tree.kt
@@ -0,0 +1,10 @@
+package com.example.treespotter_firebase
+
+import java.util.*
+
+data class Tree(
+    val name: String? = null,
+    val favorite: Boolean? = null, // nullable value if no data
+    val dateSpotted: Date? =  null)
+
+
