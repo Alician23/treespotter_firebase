2f373fea2d08670b332ce124ddeac4427ef9b657
Alicia <Alicia@gmail.com>
2023-04-09 14:07:13 -0500

    Add Google map permission

--- app/src/main/java/com/example/treespotter_firebase/TreeMapFragment.kt
+++ app/src/main/java/com/example/treespotter_firebase/TreeMapFragment.kt
@@ -1,12 +1,88 @@
 package com.example.treespotter_firebase
 
+import android.Manifest.permission.ACCESS_FINE_LOCATION
+import android.content.pm.PackageManager
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.appcompat.content.res.AppCompatResources
+import androidx.compose.runtime.internal.isLiveLiteralsEnabled
+import androidx.core.content.ContextCompat
+import androidx.lifecycle.ViewModelProvider
+import com.google.android.gms.location.FusedLocationProviderClient
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.OnMapReadyCallback
+import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.Marker
+import com.google.android.material.floatingactionbutton.FloatingActionButton
+import com.google.android.material.snackbar.Snackbar
+
+private const val TAG = "TREE_MAP_FRAGMENT"
 
 class TreeMapFragment : Fragment() {
+
+    private lateinit var addTreeButton: FloatingActionButton
+
+    private var locationPermissionGranted = false
+
+    private var movedMapToUsersLocation = false
+
+    private var fusedLocationProvider: FusedLocationProviderClient? = null
+
+    private var map: GoogleMap? = null
+
+    private val treeMarkers = mutableListOf<Marker>()
+
+    private var treeList = listOf<Tree>()
+
+    private val treeViewModel: TreeViewModel by  lazy {
+        ViewModelProvider(requireActivity()).get(TreeViewModel:: class.java)
+    }
+
+    private val mapReadyCallback = OnMapReadyCallback { googleMap ->
+
+        Log.d(TAG, "Google map ready")
+        map = googleMap
+        updateMap()
+    }
+
+    private fun updateMap() {
+        // to draw markers
+        // draw blue dot at user's location
+        // show no location  message if location permission not granted.
+        // or device does not have location enabled.
+    }
+
+    private  fun setAddTreeButtonEnabled(isEnabled: Boolean) {
+        addTreeButton.isClickable = isEnabled
+        addTreeButton.isEnabled = isEnabled
+
+        if (isEnabled) {
+            addTreeButton.backgroundTintList = AppCompatResources.getColorStateList(requireActivity(),
+                android.R.color.holo_green_light)
+
+        } else {
+            addTreeButton.backgroundTintList = AppCompatResources.getColorStateList(requireActivity(),
+            android.R.color.darker_gray)
+        }
+
+    }
+
+    private fun showSnackbar(message: String) {
+        Snackbar.make(requireView(), message, Snackbar.LENGTH_LONG).show()
+    }
+
+    private fun requestLocationPermission() {
+        // has user already granted permission?
+        if (ContextCompat.checkSelfPermission(requireActivity(),
+                Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
+            locationPermissionGranted = true
+            Log.d(TAG, "permission already granted")
+        }
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -14,6 +90,21 @@ class TreeMapFragment : Fragment() {
     ): View? {
         // Inflate the layout for this fragment
         val mainView =  inflater.inflate(R.layout.fragment_tree_map, container, false)
+
+        addTreeButton = mainView.findViewById(R.id. add_tree)
+        addTreeButton.setOnClickListener {
+            // todo add tree at user's location -if location permission granted and location available
+        }
+
+        val mapFragment =  childFragmentManager.findFragmentById(R.id.map_view) as SupportMapFragment?
+        mapFragment?.getMapAsync(mapReadyCallback)
+
+        // todo request user's permission to access device location
+
+        // todo disable add tree button until location is available
+
+        // to draw existing trees on map
+
 
         return mainView
     }
